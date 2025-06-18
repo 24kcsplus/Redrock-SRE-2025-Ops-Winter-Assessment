08f3e043c573078166d61658f88bd6a1b5b19e07
24kcsplus <1969262592@qq.com>
2025-06-18 14:52:14 +0800

    [feature] 初步完成 shell 功能

---
+++ Q3/level1/src/main.rs
@@ -0,0 +1,43 @@
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::Command;
+
+fn main(){
+    loop {
+        print!("> ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        match command {
+            "exit" => break,
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            _ => {
+                let child = Command::new(command)
+                    .args(args)
+                    .spawn();
+
+                match child {
+                    Ok(mut child) => {
+                        let _ = child.wait();
+                    },
+                    Err(e) => {
+                        eprintln!("Command failed: {}", e);
+                    }
+                }
+            }
+        }
+    }
+}
